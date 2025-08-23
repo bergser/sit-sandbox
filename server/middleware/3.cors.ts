11a4533fd3c9d41e4d94d166f60450e14c885181
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2025-08-23 18:19:19 +0200

    clean up

--- server/middleware/3.cors.ts
+++ server/middleware/3.cors.ts
@@ -1,6 +1,6 @@
 export default eventHandler((event) => {
   setResponseHeaders(event, {
-    "Access-Control-Allow-Origin": "https://play.vuejs.org", // Or specify your origin
+    "Access-Control-Allow-Origin": "https://play.vuejs.org",
     "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type",
   });
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,7 @@
+import { version, name } from "../../package.json";
 export default eventHandler((event) => {
   return `
       <meta charset="utf-8">
-      <h1>This is your brand new Nitro project 🚀 </h1>
-      <p>Get started by editing the <code>server/routes/index.ts</code> file.</p>
-      <p>Learn more from 📖 <a href="https://nitro.build/guide" target="_blank">Nitro Documentation</a></p>
+      <p>${name}: ${version}</p>
     `;
 });
