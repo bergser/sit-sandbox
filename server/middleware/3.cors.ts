13630f4dc95c2342a18e7e0178805189fa0df48e
Sergei Zheleznov <sergei.zheleznov@outlook.com>
2025-08-23 18:15:37 +0200

    clean up

--- server/middleware/3.cors.ts
+++ server/middleware/3.cors.ts
@@ -1,4 +1,7 @@
 export default eventHandler((event) => {
-  event.headers.append("Access-Control-Allow-Origin", "https://play.vuejs.org");
-  event.headers.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Origin": "https://play.vuejs.org", // Or specify your origin
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  });
 });
